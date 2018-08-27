db9f33ad2ae3d8e484a695683294022ed1ae76a8
Alexei Khatskevich <alexei.khatskevich@adesso.de>
2018-08-27 15:01:37 +0200

    bat is using maven-build-tools.jar to get configurations

--- checkstyle-check.bat
+++ checkstyle-check.bat
@@ -1,7 +1,10 @@
 
 @rem mvn install:install-file -Dfile=checkstyle-8.12-all.jar -DgroupId=com.puppycrawl.tools -DartifactId=checkstyle -Dversion=8.12-all -Dpackaging=jar
 
+@SET maven-repo=%USERPROFILE%\.m2\repository
 @SET checkstyle-version=8.12-all
-@SET checkstyle-jar=%USERPROFILE%\.m2\repository\com\puppycrawl\tools\checkstyle\%checkstyle-version%\checkstyle-%checkstyle-version%.jar
+@SET checkstyle-jar=%maven-repo%\com\puppycrawl\tools\checkstyle\%checkstyle-version%\checkstyle-%checkstyle-version%.jar
+@SET configs-version=0.1
+@SET configs-jar=%maven-repo%\net\cactusthorn\maven-build-tools\%configs-version%\maven-build-tools-%configs-version%.jar
 
-@java -Duser.language=en -DsuppressionsFile=checkstyle_suppressions.xml -jar %checkstyle-jar% -f plain -c checkstyle_checks.xml .
+@java -cp %configs-jar%;%checkstyle-jar% -Duser.language=en -DsuppressionsFile=/checkstyle_suppressions.xml com.puppycrawl.tools.checkstyle.Main -f plain -c /checkstyle_checks.xml .
